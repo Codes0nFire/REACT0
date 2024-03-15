11426cd8447bddf27661b658a756bf08b6b2fca5
NoN Responsive WebDeveloper <gg.georgemail50a@gmail.com>
2024-03-15 06:50:13 +0530

    same

--- Boilerplate/app.js
+++ Boilerplate/app.js
@@ -1,29 +1,35 @@
 const express=require("express");//require express
 const userRouter=require("./Routes/userRoute");
 
-require("./Models/dbconfig").dbconnection();//require db connection from dbconfig file
+
+//require db connection from dbconfig file
+require("./Models/dbconfig").dbconnection();
 // const dotenv=require("dotenv");
 // dotenv.config({path:"./.env"})
-require("dotenv").config({path:"./.env"});//require enviorment variables
+
+//require enviorment variables
+require("dotenv").config({path:"./.env"});
 
 const app=express(); 
 
-
-const PORT=process.env.PORT;//setting up the [port to accces
+//setting up the [port to accces
+const PORT=process.env.PORT;
 
 // logger(log the entry of routes in terminal)
 const logger = require("morgan");
 app.use(logger("tiny"));
+
 // body pasrser(cannot send post req without it)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
  
+//setting up the base route (/)
+app.use("/",userRouter)
 
-app.use("/",userRouter)//setting up the base route (/)
-
+//turn on the server on the port: PORT
 app.listen(PORT,()=>{
     console.log(`The server is running on port: ${PORT}`);
-})//turn on the server on the port: PORT
+})
 
 
